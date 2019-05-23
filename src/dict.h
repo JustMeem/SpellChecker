b39cd7d5e021a1d44de8d9b15ff8b3f62d9f9ea5
JustMeem <0_gg_0@mail.ru>
2019-05-23 17:15:19 +0700

    add header for dictonary

---
+++ src/dict.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Dict{
+    int capacity;
+    int size;
+    char** words;
+}Dict;
+
+int popDict(Dict *dict, char* str);
+Dict *loadDict(FILE *savefile);
+int saveDict(FILE *savefile, Dict *dict);
+void freeDict(Dict *dict);
