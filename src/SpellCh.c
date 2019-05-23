29919f842c7eb11962f0782cf63af4b50d15535f
TechPriestDominus <smolodtsov58@gmail.com>
2019-05-23 19:16:03 +0700

    dobavlena proverka na lisneu bukvu

--- src/SpellCh.c
+++ src/SpellCh.c
@@ -31,26 +31,64 @@ int raven(char *w1, char *w2) {
     return 1;
 }
 
+void regist(char *word)
+{
+    int reg;
+    for (reg = 0;reg < sizeof(word); reg++) {
+	if (reg > 0) {
+	    word[reg] = tolower(word[reg]);
+	}
+    }
+    printf("%s", word);
+}
+int lishb(char *word, char *eq)
+{
+    int reg, i,k;
+    char t[BUFSIZ];
+    for (reg = 0;reg < strlen(word); reg++) {
+	for (i = 0; i < reg;i++) {
+	    t[i] = word[i];
+	}
+	for (i = reg; i < strlen(word)-1;i++) {
+	    k = i;
+	    t[i] = word[i+1];
+	}
+	t[k+1] ='\0';
+	if (raven(t,eq)) {
+	    return 1;
+	}
+    }
+    return 0;
+}
 
-int SpellChecker(char *word, char *words[]) {
+int SpellChecker(char *word, char *words[], char **ret) {
     int reg;
     for (reg = 0;reg < length; reg++) {
 	if (raven(word, words[reg])) {
+	    return 1;
+	}
+    }
+    for (reg = 0;reg < length; reg++) {
+	if(lishb(word, words[reg])) {
+	    *ret = words[reg];
 	    return 0;
 	}
     }
-    return -1;
+    return 1;
 }
 int  main () {
     int reg;
     char word[BUFSIZ];
+    char *ret;
     for (reg = 0;reg < length; reg++) {
 	printf("%d%s\n",reg, words[reg]);
     }
     for(;;) {
 	scanf("%s",word);
-	if(SpellChecker(word,words) != 0) {
-	    printf("Ochibka: %s\n", word);
+	if(SpellChecker(word, words, &ret) < 1) {
+	    printf(" %s ", ret);
+	} else {
+	    printf(" %s ", word);
 	}
     }
     return 0;
