29dc4531d5b33ded5d2020bb1b19243d3da95c3f
TechPriestDominus <smolodtsov58@gmail.com>
2019-06-18 09:19:16 +0700

    ts2

--- test/dict_test.c
+++ test/dict_test.c
@@ -1,6 +1,16 @@
 #include "../src/dict.h"
-#include "../thirdparty/ctest.h"
+#include "../thirdparty/ctest.h" 
 
+CTEST(spellchecker, load)
+{
+    FILE* f_dict = fopen("dict.txt", "r+");
+    Dict* dict = loadDict(f_dict);
+    ASSERT_EQUAL(3, dict->size);
+    ASSERT_STR("first", dict->words[0]);
+    ASSERT_STR("second", dict->words[1]);
+    ASSERT_STR("third", dict->words[2]);
+    fclose(f_dict);
+}
 CTEST(spellchecker, pop)
 {
     char** words = malloc(sizeof(char*) * 3);
@@ -21,14 +31,4 @@ CTEST(spellchecker, pop)
     ASSERT_STR(dict->words[3], "word");
 
     free(words);
-}
-CTEST(spellchecker, load)
-{
-    FILE* f_dict = fopen("dict.txt", "r+");
-    Dict* dict = loadDict(f_dict);
-    ASSERT_EQUAL(3, dict->size);
-    ASSERT_STR("first", dict->words[0]);
-    ASSERT_STR("second", dict->words[1]);
-    ASSERT_STR("third", dict->words[2]);
-    fclose(f_dict);
 }
